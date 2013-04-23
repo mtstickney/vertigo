9eca563c18c0bf22eb7b0cc323cd2a431377e712
Matthew Stickney <fiendishlinux@gmail.com>
2013-04-22 23:25:31 -0400

    Add parser expansions from the prep compiler

--- parser.lisp
+++ parser.lisp
@@ -7,3 +7,28 @@
                                      (:* ,delimiter
                                          ,@(cdr args))))))
 
+(defmethod meta-sexp:transform-grammar (ret ctx (in-meta (eql t)) (directive (eql :cursor)) &optional args)
+  (declare (ignore args))
+  `(meta-sexp::parser-context-cursor ,ctx))
+
+(defmethod meta-sexp:transform-grammar (ret ctx (in-meta (eql t)) (directive (eql :data)) &optional args)
+  (declare (ignore args))
+  `(meta-sexp::parser-context-data ,ctx))
+
+(defmethod meta-sexp:transform-grammar (ret ctx (in-meta (eql t)) (directive (eql :matched-since)) &optional args)
+  `(subseq ,(meta-sexp:transform-grammar ret ctx t :data)
+           ,(first args)
+           ,(meta-sexp:transform-grammar ret ctx t :cursor)))
+
+(defmethod meta-sexp:transform-grammar (ret ctx (in-meta (eql t)) (directive (eql :with-stored-match)) &optional args)
+  (let* ((index-var (gensym))
+         (match-save (meta-sexp:transform-grammar
+                      ret ctx t :assign
+                      (list (first args)
+                            (meta-sexp:transform-grammar ret ctx t :matched-since (list index-var))))))
+    `(let ((,index-var ,(meta-sexp:transform-grammar ret ctx t :cursor)))
+       ;; (:and ,@body (:assign var (:matched-since index)))
+       ,(meta-sexp:transform-grammar ret ctx t :and
+                                     (concatenate 'list (cdr args)
+                                                  (list match-save))))))
+
