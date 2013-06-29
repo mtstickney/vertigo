55cc3a16dcb25dbf5224b48d69cc89c8dd8c3abd
Matthew Stickney <fiendishlinux@gmail.com>
2013-06-29 01:21:31 -0400

    Add restart prompting funcs

--- util.lisp
+++ util.lisp
@@ -23,3 +23,12 @@
 
 (defun stmt->assoc (s)
   (dict->assoc (statement-data s)))
+
+(defun prompt-for-val (prompt)
+  (format *query-io* (format *query-io* "~&~A: " prompt))
+  (multiple-value-list (eval (read))))
+
+(defun prompt-until (prompt &optional (test (constantly t)))
+  (loop for val = (prompt-for-val prompt)
+     until (funcall test val)
+     finally (return val)))
